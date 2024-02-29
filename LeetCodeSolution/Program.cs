b4b912a48c7aa12f9c9ab00dc846356ca2570301
NormadjonGofforov <jumab@HP>
2024-02-29 14:20:33 +0500

    20th problem solved in Leet Code

--- LeetCodeSolution/Program.cs
+++ LeetCodeSolution/Program.cs
@@ -1,15 +1,15 @@
-﻿using LeetCodeSolution.src;
-using LeetCodeSolution.src._1TwoSum;
+﻿using LeetCodeSolution.src.From1To20;
 
 class Program
 {
     public static void Main(string[] args)
     {
+        #region From 1 To 20
         #region 1. Two Sum
         int[] nums = { 2, 7, 11, 15 };
         int target = 9;
 
-        int[] result = Solution.TwoSum(nums, target);
+        int[] result = _3_Solution.TwoSum(nums, target);
 
         Console.WriteLine($"Indices: [{result[0]}, {result[1]}]");
         #endregion
@@ -32,6 +32,14 @@ class Program
         string longest2 = _14Longest.LongestCommonPrefix(strs2);
         Console.WriteLine("14. " + longest);
         Console.WriteLine("14. " + longest2);
+        #endregion
+
+        #region 20. Valid Parentheses
+        string something = "()[]{}";
+        bool isValid = _20ValidParentheses.IsValid(something);
+        Console.WriteLine(isValid);
+        #endregion
+
         #endregion
     }
 }
